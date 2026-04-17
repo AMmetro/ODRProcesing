b4c82d0d47407d431e287967e4a6f6b6a6ed00f8
AMmetro <metro.trend.now@gmail.com>
2026-04-17 11:46:06 +0300

    add init migrations and settings

---
+++ Makefile
@@ -0,0 +1,38 @@
+include .env
+export
+
+env-up:
+	docker compose up -d
+
+env-down:
+	docker compose down
+
+env-cleanup:
+	powershell -Command '$$answer = Read-Host "Delete volumes? (y/n)"; if ($$answer -eq "y" -or $$answer -eq "Y") { docker compose down -v } else { Write-Host "Cancelled" }'
+
+# env-port-forward:
+# 	docker compose run --rm ODRProcesing-port-forwarder
+	
+# env-port-close:
+# 	docker compose down port-forwarder
+
+migrate-create:
+	docker compose run --rm ODRProcesing-postgres-migrate \
+		create \
+		-ext sql \
+		-dir /migrations \
+		-seq $(seq)
+
+migrate-up:
+	make migrate-action action=up
+
+migrate-down:
+	make migrate-action action=down
+
+migrate-action:
+	docker compose run --rm \
+		--entrypoint migrate \
+		ODRProcesing-postgres-migrate \
+		-path=/migrations \
+		-database postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@ODRProcesing-postgres:5432/${POSTGRES_DB}?sslmode=disable \
+		${action}
---
+++ migrations/000001_init.up.sql
@@ -0,0 +1,28 @@
+CREATE SCHEMA ODRProcesing;
+
+CREATE TABLE ODRProcesing.users (
+    id SERIAL PRIMARY KEY,
+    VERSION BIGINT NOT NULL DEFAULT 1,
+    full_name VARCHAR(100) NOT NULL CHECK (char_length(full_name) BETWEEN 3 AND 100),
+    phone_number VARCHAR(15) CHECK (
+        phone_number ~ '^\+[0-9]+$'
+        AND char_length(phone_number) BETWEEN 10 AND 15
+        )
+);
+
+CREATE TABLE ODRProcesing.tasks (
+    id SERIAL PRIMARY KEY,
+    version BIGINT NOT NULL DEFAULT 1,
+    title VARCHAR(100) NOT NULL CHECK(char_length(title) BETWEEN 1 AND 100),
+    description VARCHAR(1000) CHECK(char_length(description) BETWEEN 1 AND 1000),
+    completed BOOLEAN NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    completed_at TIMESTAMP,
+    author_user_id INTEGER NOT NULL REFERENCES ODRProcesing.users(id),
+    
+    CHECK (
+        (completed=FALSE AND completed_at IS NULL)
+        OR
+        (completed=TRUE AND completed_at IS NOT NULL AND completed_at >= created_at)
+    )
+);
